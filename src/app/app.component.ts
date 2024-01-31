cc91901bfce9f8089f734358ff017b5a57aca8f1
Poojabodake2001 <poojabodake5284@gmail.com>
2024-01-31 19:29:40 +0530

    radio-toggle-task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'task-1';
+
+  showFirstDiv : boolean  = true;
+  showSecondDiv: boolean = false;
+  toggleDiv(selectedValue: string){
+    this.showFirstDiv= selectedValue === 'first';
+    this.showSecondDiv = selectedValue === 'second';
+  }
+
 }
